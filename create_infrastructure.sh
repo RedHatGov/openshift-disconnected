d5ec5411eb7a183b8fc2267d084e5b7ba87ef8f3
Jason Callaway <jason@jasoncallaway.com>
2016-06-02 13:13:03 -0400

    Testing bastion host logic

--- create_infrastructure.sh
+++ create_infrastructure.sh
@@ -2,8 +2,8 @@
 
 source env_aws.sh
 
-printf "Refreshing EC2 cache..."
-./inventory/aws/hosts/ec2.py --refresh-cache
-printf "done\n"
+#printf "Refreshing EC2 cache..."
+#./inventory/aws/hosts/ec2.py --refresh-cache
+#printf "done\n"
 
 ansible-playbook -v -i inventory/aws/hosts/ec2.py create_infrastructure.yaml
