9d619acccca12c9edae8d0feef5990d312959a9a
Jason Callaway <jason@jasoncallaway.com>
2016-06-02 12:48:01 -0400

    Making naming more consistent, and shortening name strings

--- env_aws.sh
+++ env_aws.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export ec2_region='us-east-1'
-export os_cluster_id='openshift-disconnected'
+export os_cluster_id='ose-disconn'
 export ec2_public_az="${ec2_region}a"
 export ec2_private_1a_az="${ec2_region}a"
 export ec2_private_1e_az="${ec2_region}e"
