66c75be58efac84dd17acb5f951c8ae2cba2cedd
Jason Callaway <jason@jasoncallaway.com>
2016-06-05 12:51:25 -0500

    Added Elastic IP and Route53 sections

--- env_aws.sh
+++ env_aws.sh
@@ -8,4 +8,5 @@ export ec2_private_1a_az="${ec2_region}a"
 export ec2_private_1e_az="${ec2_region}e"
 export ec2_image="ami-2051294a"
 export ec2_instance_type_bastion="m4.large"
+export r53_zone="rhtps.io"
 
