f843fe1153e76cc37d4f9b3d0a968fc70450bb0c
mshoger <mshoger@redhat.com>
2016-06-08 22:06:35 -0400

    Adding additional DNS config files.

--- env_aws.sh
+++ env_aws.sh
@@ -2,11 +2,13 @@
 
 export ANSIBLE_HOST_KEY_CHECKING="False"
 export ec2_region='us-east-1'
-export os_cluster_id='ose-disconn'
+export os_cluster_id='ose-dns-test'
 export ec2_public_az="${ec2_region}a"
 export ec2_private_1a_az="${ec2_region}a"
 export ec2_private_1e_az="${ec2_region}e"
 export ec2_image="ami-2051294a"
 export ec2_instance_type_bastion="m4.large"
+export ec2_instance_type_dnsldap="m4.large"
+export ec2_instance_type_ose_node="m4.large"
 export r53_zone="rhtps.io"
 
