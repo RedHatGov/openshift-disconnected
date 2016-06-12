ae94a5cda68bff618b898475bc8fea69ecdc7db9
Jason Callaway <jason@jasoncallaway.com>
2016-06-12 14:26:10 -0400

    Tweaks to new logic.

--- roles/bastion/files/docker_dump.sh
+++ roles/bastion/files/docker_dump.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+destdir='/var/www/html'
+
 for image in `docker images | grep -v ^REPOSITORY | '{print $1":"$2}'`
 do
     echo ${image}
-    docker save ${image} > ${image}.tar
+    docker save ${image} > ${destdir}/${image}.tar
 done
