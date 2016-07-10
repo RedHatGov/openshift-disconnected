7a64c306454c6ee2ed18591c5aa1995c6cbca8fa
Jason Callaway <jason@jasoncallaway.com>
2016-07-10 12:50:33 -0400

    VNC works with the exception of starting the service. A manual workaround is listed in vnc.yml. Moving on.

--- sendit.sh
+++ sendit.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 ansible-playbook -i inventory/aws/hosts/ec2.py site.yml
+# The following two lines are a workaround to ec2 dynamic inventory problems
+# until I figure out a more elegant solution.
+python inventory/aws/hosts/ec2.py --refresh-cache
+ansible-playbook -i inventory/aws/hosts/ec2.py site.yml --skip-tags=private_aws
