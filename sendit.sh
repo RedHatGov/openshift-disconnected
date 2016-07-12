02efe859f06f4e6a3865dc3863f8786840999f04
Jason Callaway <jason@jasoncallaway.com>
2016-07-12 13:41:33 -0400

    Had routes to the peering connection backwards

--- sendit.sh
+++ sendit.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-ansible-playbook -i inventory/aws/hosts/ec2.py site.yml
-# The following two lines are a workaround to ec2 dynamic inventory problems
-# until I figure out a more elegant solution.
-python inventory/aws/hosts/ec2.py --refresh-cache
-ansible-playbook -i inventory/aws/hosts/ec2.py site.yml --skip-tags=private_aws
+#ansible-playbook -i inventory/aws/hosts/ec2.py site.yml
+## The following two lines are a workaround to ec2 dynamic inventory problems
+## until I figure out a more elegant solution.
+#python inventory/aws/hosts/ec2.py --refresh-cache
+#ansible-playbook -i inventory/aws/hosts/ec2.py site.yml --skip-tags=private_aws
 
 # At this point our bastion host should be up, so we'll proceed with
 # the private hosts via the bastion.
