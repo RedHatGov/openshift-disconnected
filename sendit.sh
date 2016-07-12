dfad0a5fea4c946dad82913dc0f01de9e36a3823
Jason Callaway <jason@jasoncallaway.com>
2016-07-12 17:36:08 -0400

    Bind works, onto the OSE install!

--- sendit.sh
+++ sendit.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 export ANSIBLE_CONFIG=./ansible.cfg
-#ansible-playbook -i inventory/aws/hosts/ec2.py site.yml
-## The following two lines are a workaround to ec2 dynamic inventory problems
-## until I figure out a more elegant solution.
-#python inventory/aws/hosts/ec2.py --refresh-cache
-#ansible-playbook -i inventory/aws/hosts/ec2.py site.yml --skip-tags=private_aws
-
+ansible-playbook -i inventory/aws/hosts/ec2.py site.yml && \
+# The following two lines are a workaround to ec2 dynamic inventory problems
+# until I figure out a more elegant solution.
+python inventory/aws/hosts/ec2.py --refresh-cache && \
+ansible-playbook -i inventory/aws/hosts/ec2.py site.yml --skip-tags=private_aws &&\
 # At this point our bastion host should be up, so we'll proceed with
 # the private hosts via the bastion.
-#python inventory/aws/hosts/ec2.py --refresh-cache
-export ANSIBLE_CONFIG=./ansible_bastion.cfg
+export ANSIBLE_CONFIG=./ansible_bastion.cfg && \
 ansible-playbook -i inventory/static/hosts static.yml
