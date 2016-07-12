096cfd52ec8c343509fd33962e401b0c20c83ccd
Jason Callaway <jason@jasoncallaway.com>
2016-07-11 21:32:21 -0400

    Added support for the ansible-bind role on internal instances. Lots of changes, all untested.

--- sendit.sh
+++ sendit.sh
@@ -5,3 +5,9 @@ ansible-playbook -i inventory/aws/hosts/ec2.py site.yml
 # until I figure out a more elegant solution.
 python inventory/aws/hosts/ec2.py --refresh-cache
 ansible-playbook -i inventory/aws/hosts/ec2.py site.yml --skip-tags=private_aws
+
+# At this point our bastion host should be up, so we'll proceed with
+# the private hosts via the bastion.
+python inventory/aws/hosts/ec2.py --refresh-cache
+ANSIBLE_CONFIG=./ansible_bastion.cfg \
+  ansible-playbook -i inventory/static/hosts static.yml
