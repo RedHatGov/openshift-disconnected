e5d4245e50e1456e53ebaa346730ea2d47a7da0b
Jason Callaway <jason@jasoncallaway.com>
2016-07-15 18:18:45 -0400

    Tweaked env script, fixed problem with hosts template, and added firewalld rich rules

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Use the correct PYTHONPATH on Mac OS X
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname -s)" == "Darwin" ]; then
     export PYTHONPATH=/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages:/usr/local/lib/python2.7/site-packages
 fi
 
