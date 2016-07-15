78341211676a92f38b2f6b06c14af303bf7aedfd
Jason Callaway <jason@jasoncallaway.com>
2016-07-15 18:23:37 -0400

    Added step to regenerate static hosts in case they've been powered off and IP addresses have changed.

--- sendit.sh
+++ sendit.sh
@@ -5,8 +5,10 @@ ansible-playbook -i inventory/aws/hosts/ec2.py site.yml && \
 # The following two lines are a workaround to ec2 dynamic inventory problems
 # until I figure out a more elegant solution.
 python inventory/aws/hosts/ec2.py --refresh-cache && \
-ansible-playbook -i inventory/aws/hosts/ec2.py site.yml --skip-tags=private_aws &&\
+ansible-playbook -i inventory/aws/hosts/ec2.py site.yml --skip-tags=private_aws && \
+# Regenerate static host inventory
+ansible-playbook -i inventory/aws/hosts/ec2.py site.yml --tags=static_hosts && \
 # At this point our bastion host should be up, so we'll proceed with
-## the private hosts via the bastion.
+# the private hosts via the bastion.
 export ANSIBLE_CONFIG=./ansible_bastion.cfg && \
 ansible-playbook -i inventory/static/hosts static.yml
